b4cc02cde52e47264db5d750df9a63112a50acef
ksure <kyle.scherling@gmail.com>
2018-06-10 16:03:17 -0700

    Adds basic theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import 'normalize.css'
 import 'styles/global'
+import App from './App'
+import { ThemeProvider } from 'emotion-theming'
+import theme from 'styles/theme'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const colors = {
+  white: '#ffffff',
+  offWhite: '#f2f2f2',
+  ltGray: '#cccccc',
+  mdGray: '#999999',
+  dkGray: '#333333'
+}
+
+export default {
+  colors
+}
