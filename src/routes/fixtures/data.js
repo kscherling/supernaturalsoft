b20f974d14d7ce43b2ba41e3946fff4cd0d3e89c
ksure <kyle.scherling@gmail.com>
2018-06-10 23:58:19 -0700

    Adds Fixtures

---
+++ src/routes/fixtures/data.js
@@ -0,0 +1,21 @@
+export const languages = [
+  'Elixir',
+  'Ruby',
+  'JS',
+  'Rails',
+  'Phoenix',
+  'React',
+  'Ember',
+  'AWS'
+]
+
+export const practices = [
+  'Agile',
+  'Cloud-based',
+  'Continuous Integration',
+  'Production Readiness',
+  'Rapid-prototyping',
+  'User-centered'
+]
+
+export const clients = ['Nike', 'Chegg', 'Pandora', 'Netflix']
